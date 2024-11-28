b0a08ae04c4b82df3ffde4917ec6d46417178e89
SteadfastInternational <hope08136@gmail.com>
2024-11-28 07:56:17 -0800

    cors

--- src/app.js
+++ src/app.js
@@ -4,11 +4,23 @@ const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
 const OrderRoutes = require('./routes/OrderRoutes');
-const productController = require('./controllers/productController')
+const productController = require('./controllers/productController');
+const cors = require('cors'); // Import the CORS package
+
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Configure CORS to allow requests from 'https://www.steadfast.ng'
+const corsOptions = {
+  origin: 'https://www.steadfast.ng',  // Specify the allowed origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+};
+
+// Enable CORS with the configured options
+app.use(cors(corsOptions));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
