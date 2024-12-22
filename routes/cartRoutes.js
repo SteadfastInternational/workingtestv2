58c9fabc931c69fdd25407f2e7e128f28389647e
SteadfastInternational <hope08136@gmail.com>
2024-12-22 04:35:10 -0800

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -11,6 +11,6 @@ router.post("/cart", protect, CartController.createCart);
 router.get("/carts", isAdmin, CartController.getAllCarts);
 
 // Route to fetch a specific cart by ID (admin-only)
-router.get("/cart/:id", isAdmin, CartController.getCartById);
+router.get("/cart/:id",  CartController.getCartById);
 
 module.exports = router;
